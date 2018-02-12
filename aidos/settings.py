2c6a781e1024226049b9139a64a8fdb78524d503
Mathieu <mathieucarre@icloud.com>
2018-02-12 20:37:19 +0100

    Modification DEBUG

--- aidos/settings.py
+++ aidos/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zg2opxglwvrn!!*2r*$7($6zw&j*m(pq2#c%8k&m=4nzk4f5i2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'aidos.pythonanywhere.com']
 
